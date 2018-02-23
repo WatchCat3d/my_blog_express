21f2e049ce813e192733d43011d4947a71467c34
WatchCat <810549046@qq.com>
2018-02-23 21:14:21 +0800

    initialize express project code

---
+++ routes/regist.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.post('/', function(req, res, next) {
+    var post = req.body;
+    console.log(post);
+    res.redirect("http://localhost:3000/");
+});
+
+module.exports = router;
