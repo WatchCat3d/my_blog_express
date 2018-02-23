2ba95a05a1d05d4fcf6b7756b9594c536c680df6
WatchCat <810549046@qq.com>
2018-02-24 00:16:41 +0800

    create user data handler api

--- model/User_handle.js
+++ model/User_handle.js
@@ -3,14 +3,74 @@ var User = require('./User_model');
 
 var url = "mongodb://localhost:27017/users";
 
+function db_connect() {
+    mongoose.connect(url, function (err) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            console.log("数据库已连接");
+        }
+    });
+}
+
 module.exports = {
-    db_connect: function () {         
-        mongoose.connect(url, function (err) {
+    user_save: function (user) {
+        db_connect();
+
+        new User({
+            username: user.username,
+            password: user.password,
+            email: user.email,
+            regist_date: user.regist_date
+        }).save(function (err, res) {
             if (err) {
                 console.log(err);
             }
             else {
-                console.log("数据库已连接");
+                console.log(res);
+            }
+        });
+
+    },
+    user_count: function (pattem) {
+        db_connect();
+        
+        User.count(pattem, function (err, res) {
+            if (err) {
+                console.log(err);
+                return false;
+            }
+            else {
+                console.log(res);
+                return res;
+            }
+        });
+    },
+    user_search: function (pattem) {
+        db_connect();
+
+        User.find(pattem, function (err, res) {
+            if (err) {
+                console.log(err);
+                return false;
+            }
+            else {
+                console.log(res);
+                return res;
+            }
+        });
+    },
+    user_delete: function (pattem) {
+        db_connect();
+        User.remove(pattem, function (err, res) {
+            if (err) {
+                console.log(err);
+                return false;
+            }
+            else {
+                console.log(res);
+                return res;
             }
         });
     }
--- routes/regist.js
+++ routes/regist.js
@@ -5,8 +5,19 @@ var router = express.Router();
 /* GET users listing. */
 router.post('/', function(req, res, next) {
     var post = req.body;
-    console.log(post);
-    res.send("success");
+
+    /*
+    if (db.user_count({username: post.username}) > 0) {
+        res.send("exist_username");
+    }
+    else if (db.user_count({email: post.email}) > 0) {
+        res.send("exist_email");
+    }
+    else {
+        console.log(db.user_count({username: post.username}));
+        db.user_save(post);
+        res.send("success");
+    }*/
 });
 
 module.exports = router;
