a1281d91a32918ae1054a12c9c7a19e641ea36a0
WatchCat <810549046@qq.com>
2018-06-07 14:27:25 +0800

    把前端的页面跳转利用localhost，全改为router.push

--- routes/blog_content.js
+++ routes/blog_content.js
@@ -3,7 +3,7 @@ var db = require('../model/Blog_handle');
 var router = express.Router();
 
 /* GET home page. */
-router.post('/', function(req, res, next) {
+router.post('/', function (req, res, next) {
     var post = req.body;
     db.blog_search(post).then(function (result) {
         res.send(result[0]);
