a5e1299084133cd5dd27bc8478db6d410160d48e
WatchCat <810549046@qq.com>
2018-06-22 20:52:41 +0800

    现在个人的评论页已做好，且删除博客，博客下的评论一并删除

--- routes/blog_delete.js
+++ routes/blog_delete.js
@@ -1,13 +1,20 @@
 var express = require('express');
 var db = require('../model/Blog_handle');
+var db2 = require('../model/BlogComment_handle');
 var router = express.Router();
 
-/* GET home page. */
+//删除博客时，要把博客的评论一并删除
 router.post('/', function (req, res, next) {
     var post = req.body;
-    db.blog_delete(post).then(function (result) {
-        res.send(result);
-    });
+    var post2 = {
+        blog_writer: post.writer,
+        blog_date: post.blog_date
+    }
+    db2.blogComment_delete(post2).then(function (result) {
+        db.blog_delete(post).then(function (result) {  
+            res.send(result);
+        });
+    })
 });
 
 module.exports = router;
